376c7517f5c5de130922a4220bb57031434e5502
Ankush Menat <ankushmenat@gmail.com>
2020-08-02 16:50:46 +0530

    Use nerd fonts for icons

--- .config/xmobar/xmobar.hs
+++ .config/xmobar/xmobar.hs
@@ -1,4 +1,4 @@
-Config { font = "xft:JetBrains Mono:pixelsize=14:antialias=true:hinting=true"
+Config { font = "xft:JetBrainsMono Nerd Font:pixelsize=14:antialias=true:hinting=true"
        , additionalFonts = []
        , borderColor = "black"
        , bgColor = "#282A36"
@@ -12,17 +12,14 @@ Config { font = "xft:JetBrains Mono:pixelsize=14:antialias=true:hinting=true"
        , hideOnStart = False
        , allDesktops = True
        , overrideRedirect = True
-       , commands = [ Run Network "enp1s0" ["-L","0","-H","32",
-                                          "--normal","#8BE9FD","--high","#FF5555"] 50
-                    , Run Cpu ["-t", "c: <total>%","-H","70","--high","#FF5555"] 50
-                    , Run Memory ["-t","m: <usedratio>%"] 50
-                    , Run Swap [] 10
-                    , Run Com "uname" ["-s","-r"] "" 36000
-                    , Run Date "%a %b %_d %Y %I:%M" "date" 300
+       , commands = [ Run Network "enp1s0" ["-t", " <rx>  <tx>"] 50
+                    , Run Cpu ["-t", " <total>%","-H","70","--high","#FF5555"] 50
+                    , Run Memory ["-t"," <usedratio>%"] 50
+                    , Run Date "<fc=#50FA7B> %a %b %d %Y %I:%M </fc>" "date" 300
                     , Run StdinReader
                     ]
        , sepChar = "%"
        , alignSep = "}{"
        , template = " <icon=haskell.xpm/>  %StdinReader% }\
-                    \{ <fc=#F1FA8C>%cpu%</fc> | <fc=#FF6E67>%memory%</fc> |<fc=#8BE9FD> %enp1s0%</fc> | <fc=#50FA7B>%date%</fc> "
+                    \{ <fc=#F1FA8C>%cpu%</fc> | <fc=#FF6E67>%memory%</fc> |<fc=#8BE9FD> %enp1s0%</fc> | %date% "
        }
