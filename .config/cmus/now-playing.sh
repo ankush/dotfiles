0f88a0fc89726e4c147321b65e6c8a782b97db30
Ankush Menat <ankushmenat@gmail.com>
2020-08-14 10:12:43 +0530

    cmus script: Set max length of artist names

--- .config/cmus/now-playing.sh
+++ .config/cmus/now-playing.sh
@@ -5,6 +5,12 @@ then
     cmusstatus=$(cmus-remote -Q)
     artist=$(echo -n $(grep artist -m 1 <<< "$cmusstatus" | cut -c 12-))
     song=$(echo -n $(grep title -m 1 <<< "$cmusstatus" | cut -c 11-))
+
+    if [ ${#artist} -ge 30 ]
+    then
+        artist=${artist:0:26}
+        artist+=" ..."
+    fi
     echo -n " $artist ~ $song"
 else
     echo -n "ﱙ "
