da1f139ca756efb99badda601613b1b432f5fb8b
Ankush Menat <ankushmenat@gmail.com>
2020-09-21 20:25:14 +0530

    cmus: remove usesless echo from now-playing.sh

--- .config/cmus/now-playing.sh
+++ .config/cmus/now-playing.sh
@@ -3,15 +3,21 @@
 if pgrep -x "cmus" > /dev/null
 then
     cmusstatus=$(cmus-remote -Q)
-    artist=$(echo -n $(grep artist -m 1 <<< "$cmusstatus" | cut -c 12-))
-    song=$(echo -n $(grep title -m 1 <<< "$cmusstatus" | cut -c 11-))
+    artist="$(grep artist -m 1 <<< "$cmusstatus" | cut -c 12-)"
+    song="$(grep title -m 1 <<< "$cmusstatus" | cut -c 11-)"
 
-    if [ ${#artist} -ge 30 ]
+    if [ ${#artist} -ge 25 ]
     then
-        artist=${artist:0:28}
+        artist=${artist:0:23}
         artist+=" …"
     fi
-    echo -n " $artist ~ $song"
+    if [ ${#song} -ge 25 ]
+    then
+        song=${song:0:23}
+        song+=" …"
+    fi
+
+    echo -n " $artist — $song"
 else
     echo -n "ﱙ "
 fi
