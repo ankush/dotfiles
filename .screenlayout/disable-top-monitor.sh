127f29eacad55a88c7d1ec7c3827f743f72cc671
Ankush Menat <ankushmenat@gmail.com>
2021-03-21 19:38:37 +0530

    stop tracking screenlayouts

--- .screenlayout/disable-top-monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-0 --off --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-0 --off
--- .screenlayout/dual-monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DVI-D-0 --off
