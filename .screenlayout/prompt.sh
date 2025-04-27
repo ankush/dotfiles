8cc604fdf0336b8c7037b7f233df45c6d1ef99e0
Ankush Menat <ankushmenat@gmail.com>
2025-04-27 13:29:46 +0530

    fix: show nagbar on primary always

--- .screenlayout/prompt.sh
+++ .screenlayout/prompt.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+
+# Attaching diff kb requires this for some reason
+xset r rate 280 60
+setxkbmap -option caps:escape
+
 num_monitors=$(xrandr | grep -sw 'connected' | wc -l)
 if [[ "$num_monitors" == 1 ]] ; then
 	xrandr --output eDP --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off
 	exit
 fi
 
-i3-nagbar -m "Pick Display Configurtion" -t warning \
+i3-nagbar -p -m "Pick Display Configurtion" -t warning \
 	-b "Inbuilt"  "xrandr --output eDP --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off" \
 	-b "Secondary"  "xrandr --output eDP --off --output HDMI-A-0 --off --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal" \
 	-b "Both"  "xrandr --output eDP --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --mode 1920x1080 --pos 0x60 --rotate normal"
 
 
-# Attaching diff kb requires this for some reason
-xset r rate 280 60
-setxkbmap -option caps:escape
